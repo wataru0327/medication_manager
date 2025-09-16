2b6a05a2724102899539428a3d27ab6b17cb1ee3
wataru0327 <watarurataw00@icloud.com>
2025-09-16 17:47:52 +0900

    medicationモデルにアソシエーション追加

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,2 +1,8 @@
 class Medication < ApplicationRecord
+  belongs_to :prescription
+
+  enum timing: { morning: 0, noon: 1, evening: 2, bedtime: 3 }
+
+  validates :name, :dosage, :timing, presence: true
 end
+
