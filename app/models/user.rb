1277f23f2553531313781f3886f9c0fc15da8cd6
wataru0327 <watarurataw00@icloud.com>
2025-09-16 17:45:09 +0900

    userモデルにアソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  enum role: { doctor: 0, pharmacy: 1, patient: 2 }
+
+  has_many :prescriptions, foreign_key: :doctor_id, dependent: :destroy, inverse_of: :doctor
+  has_many :prescriptions, foreign_key: :patient_id, dependent: :destroy, inverse_of: :patient
+  has_many :status_updates, foreign_key: :pharmacy_id, dependent: :destroy, inverse_of: :pharmacy
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
