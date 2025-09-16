35a60b092fa6cd072bfe3236951843497e0b0bd6
wataru0327 <watarurataw00@icloud.com>
2025-09-16 17:48:41 +0900

    status_updateモデルにアソシエーション追加

--- app/models/status_update.rb
+++ app/models/status_update.rb
@@ -1,2 +1,9 @@
 class StatusUpdate < ApplicationRecord
+  belongs_to :prescription
+  belongs_to :pharmacy, class_name: "User"
+
+  enum status: { pending: 0, processing: 1, completed: 2 }
+
+  validates :prescription_id, :pharmacy_id, :status, presence: true
 end
+
