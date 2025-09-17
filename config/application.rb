a3779faad62cd1a86166f9266a0190406ab8af6c
wataru0327 <watarurataw00@icloud.com>
2025-09-17 16:58:28 +0900

    プルダウン修正と英語の箇所を日本語に変更

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative "boot"
 
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module MedicationManager
@@ -11,17 +9,14 @@ module MedicationManager
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    # lib配下のオートロード設定
     config.autoload_lib(ignore: %w(assets tasks))
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # -----------------------------
+    # 日本語化設定を追加
+    # -----------------------------
+    config.i18n.default_locale = :ja   # デフォルトを日本語に
+    config.i18n.available_locales = [:en, :ja]  # 利用可能な言語
+    config.time_zone = 'Tokyo'         # タイムゾーンを日本に
   end
 end
