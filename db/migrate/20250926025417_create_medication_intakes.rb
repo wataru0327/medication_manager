70f603e3ddc26fc9c5523312d31e6d16dc495748
wataru0327 <watarurataw00@icloud.com>
2025-09-30 14:08:51 +0900

    migration ファイル重複修正

--- db/migrate/20250926025417_create_medication_intakes.rb
+++ db/migrate/20250926025417_create_medication_intakes.rb
@@ -1,17 +1,11 @@
 class CreateMedicationIntakes < ActiveRecord::Migration[7.1]
   def change
-  
-  end
-end
-
-class CreateMedicationIntakes < ActiveRecord::Migration[7.1]
-  def change
     create_table :medication_intakes do |t|
       t.references :user, null: false, foreign_key: true
       t.references :prescription_item, null: false, foreign_key: true
       t.datetime :taken_at, null: false
-
       t.timestamps
     end
   end
 end
+
